11c318032ebfbc39858536e3cc7891d996f1cc39
Abd Hadi Hussein <abdhadi.hussein@petronas.com>
2024-07-02 17:22:05 +0800

    added js tutorial

---
+++ main.js
@@ -0,0 +1,15 @@
+let number = 0;
+number = 1;
+
+const sym1 = Symbol("foo");
+
+const used = 4.53;
+
+let value = {
+    "price" : 2.5,
+    "total" : 10,
+}
+
+console.log(value["price"]);
+
+console.log(typeof(sym1));
